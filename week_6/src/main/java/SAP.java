f8b6a0b314403802b270bbb9cefa4b690b23f964
ericghara <eric.gharakhanian@gmail.com>
2021-10-28 03:46:04 -0700

    SAP ready for unit testing

--- week_6/src/main/java/SAP.java
+++ week_6/src/main/java/SAP.java
@@ -1,2 +1,170 @@
-package PACKAGE_NAME;public class SAP {
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+
+import java.util.*;
+
+public class SAP {
+    private final Digraph DAG;
+    // constructor takes a digraph (not necessarily a DAG)
+    public SAP(Digraph G) {
+        errorOnNull(G);
+        new ErrorOnCycle(G);
+        DAG = new Digraph(G);
+
+    }
+
+    // length of shortest ancestral path between v and w; -1 if no such path
+    public int length(int v, int w) {
+        Integer[] vL = {v};
+        Integer[] wL = {w};
+        return length(Arrays.asList(vL), Arrays.asList(wL) );
+    }
+
+    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+    public int ancestor(int v, int w) {
+        Integer[] vL = {v};
+        Integer[] wL = {w};
+        return ancestor(Arrays.asList(vL), Arrays.asList(wL) );
+    }
+
+    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+    public int length(Iterable<Integer> v, Iterable<Integer> w) {
+        MultiBFS bfs = new MultiBFS(v,w);
+        return bfs.getLength();
+    }
+
+    // a common ancestor that participates in shortest ancestral path; -1 if no such path
+    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+        MultiBFS bfs = new MultiBFS(v,w);
+        return bfs.getAncestor();
+    }
+
+    private void errorOnNull(Object obj) {
+        if (obj == null) {
+            throw new IllegalArgumentException("Received a null input.");
+        }
+    }
+
+    static class ErrorOnCycle {
+        boolean[] seen;
+        Digraph G;
+
+        ErrorOnCycle(Digraph G) {
+            this.G = G;
+            seen = new boolean[G.V()];
+            for (int i = 0; i < seen.length; i++) {
+                if (dfs(i, i)) {
+                    throw new IllegalArgumentException("Cycle found: Digraph is not a DAG.");
+                }
+            }
+        }
+
+        // returns true if a cycle is detected
+        boolean dfs(int v, int origin) {
+            if (seen[v]) {
+                return false;
+            }
+            for (int w : G.adj(v)) {
+                if (w == origin || dfs(w, origin)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+        // Finds first intersection of 2 groups of synsets (input as a collection)
+        // Necessary b/c 1 noun can map to multiple synsets (meanings):
+        // ie "passing" maps to: 59442, 59443, 59444, 59445 , 59446
+        private class MultiBFS {
+            private int[] pathA, pathB;   // Path to common Ancestor
+            private int ancestor;
+            private int length;
+            HashMap<Integer, Integer> aPath;
+            HashMap<Integer, Integer> bPath;
+
+            public MultiBFS(Iterable<Integer> A, Iterable<Integer> B) {
+                ancestor = -1; // if no common ancestor -1
+                bfs(A, B);
+                followPath();
+            }
+
+            // Retraces BFS to find shortest path implemented for testing
+            private void tracePath() {
+
+            }
+
+            public int getAncestor() {return ancestor; }
+
+            public int getLength() {return length; }
+
+            private void followPath() {
+                if (ancestor > -1) {
+                    length = 0;
+                    for (int nxt = ancestor; aPath.containsKey(nxt); nxt = aPath.get(nxt) ) {
+                        length++; }
+                    for (int nxt = ancestor; bPath.containsKey(nxt); nxt = bPath.get(nxt) ) {
+                        length++; }
+                }
+                else { length = -1; }
+            }
+
+            private void bfs(Iterable<Integer> A, Iterable<Integer> B) {
+                LinkedList<Integer> aQ = new LinkedList<>();
+                LinkedList<Integer> bQ = new LinkedList<>();
+                HashSet<Integer> aSeen = new HashSet<>();
+                HashSet<Integer> bSeen = new HashSet<>();
+                aPath = new HashMap<>();
+                bPath = new HashMap<>();
+                A.forEach((v) -> { aQ.add(v); aSeen.add(v); } );
+                B.forEach((v) -> { bQ.add(v); bSeen.add(v); } );
+
+                while (!aQ.isEmpty() || !bQ.isEmpty()) {
+                    int size = aQ.size();
+                    for (int i = 0; i < size; i++) {
+                        int cur = aQ.pop();
+                        for (int nbr : DAG.adj(cur)) {
+                            if (aSeen.add(nbr)) {
+                                aQ.addLast(nbr);
+                                aPath.put(nbr, cur);
+                                if (bSeen.contains(nbr)) {
+                                    aQ.clear(); bQ.clear();
+                                    ancestor = nbr;
+                                    size = 0; // clean up how this exits
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    size = bQ.size();
+                    for (int i = 0; i < size; i++) {
+                        int cur = bQ.pop();
+                        for (int nbr : DAG.adj(cur)) {
+                            if (bSeen.add(nbr)) {
+                                bQ.addLast(nbr);
+                                bPath.put(nbr, cur);
+                                if (aSeen.contains(nbr)) {
+                                    aQ.clear(); bQ.clear();
+                                    ancestor = nbr;
+                                    size = 0;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    // hypfile format: # vertices, followed by # edges, pairs of vertices, (each entry separated by whitespace)
+    public static void main(String[] args) {
+        int aVert = Integer.parseInt(args[1]);
+        int bVert = Integer.parseInt(args[2]);
+        if (args.length != 3) { throw new IllegalArgumentException("Invalid input.  Input should be path to hypernyms.txt"); }
+        In hypFile = new In(args[0]);
+        Digraph G = new Digraph(hypFile);
+        SAP sap = new SAP(G);
+        System.out.printf("Distance from %s to %s: %8d%n", aVert, bVert,sap.length(bVert, aVert));
+        System.out.printf("Closest Ancestor of %1$s and %2$s: %3$d%n", aVert, bVert,sap.ancestor(bVert, aVert));
+    }
 }
--- week_6/src/main/java/WordNet.java
+++ week_6/src/main/java/WordNet.java
@@ -1,15 +1,16 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Digraph;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.stream.IntStream;
 
 public class WordNet {
 
-    private final HashMap<String,Integer> ST = new HashMap<>();
+    private final HashMap<String,LinkedList<Integer> > ST = new HashMap<>();
     final private ArrayList<String> keys = new ArrayList<>();
     final private Digraph G;
+    final private SAP sap;
 
     // constructor takes the name of the two input files
     // Hypernym more general: go up graph
@@ -17,28 +18,79 @@ public class WordNet {
     // Hypernym: child,parent,parent...
     // Synset: id,word,gloss (definition)
     public WordNet(String synsets, String hypernyms) {
-        isNull(synsets); isNull(hypernyms);
-        // Parse synsets
-        In synFile = new In(synsets);
+        errorOnNull(synsets); errorOnNull(hypernyms);
+        parseSynsets(synsets);
+        G = new Digraph(keys.size());
+        parseHypernyms(hypernyms);
+        sap = new SAP(G);
+    }
+
+    // returns all WordNet nouns
+    public Iterable<String> nouns() {
+        // Defensive copy: edits to keySet can affect HasMap
+        String[] ks = ST.keySet().toArray(new String[0]);
+        return Arrays.asList(ks);
+    }
+
+    // is the word a WordNet noun?
+    public boolean isNoun(String word) {
+        errorOnNull(word);
+        return ST.containsKey(word);
+    }
+
+    // distance between nounA and nounB (defined below)
+    public int distance(String nounA, String nounB) {
+        nounError(nounA, nounB);
+        LinkedList <Integer> idsA = ST.get(nounA);
+        LinkedList <Integer>  idsB = ST.get(nounB);
+        return sap.length(idsA, idsB);
+    }
+
+    // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+    // in a shortest ancestral path
+    public String sap(String nounA, String nounB) {
+        nounError(nounA, nounB);
+        LinkedList<Integer> idsA = ST.get(nounA);
+        LinkedList<Integer> idsB = ST.get(nounB);
+        int papa = sap.ancestor(idsA, idsB);
+        if (papa == -1) {
+            return String.format("%s and %s are not connected.", nounA, nounB);
+        }
+        return keys.get(papa);
+    }
+
+    // Input csv: id,word,gloss (gloss is unused)
+    private void parseSynsets (String file) {
+        In synFile = new In(file);
         while (synFile.hasNextLine()) {
-            // csv: id,word,gloss (definition) (gloss is unused)
             String[] line = synFile.readLine().split(",");
-            if (line.length < 2) {  continue; } // implement multiple root detection here
+            if (line.length < 2) {
+                continue;
+            } // implement multiple root detection here
             int i = Integer.parseInt(line[0]);
-            //String[] ks = line[1].split(" ");
-            String ks = line[1];
             if (keys.size() != i) {
                 throw new IllegalArgumentException("Found a Key : ID mismatch in synset input file at line: " + keys.size());
             }
-            //keys.add(ks[0]);
-            keys.add(ks);
-            //for (String k : ks) { ST.put(k, i);}
-            ST.put(ks, i);
+            String[] ks = line[1].split(" ");
+            keys.add(ks[0]);
+            for (String k : ks) {
+                LinkedList<Integer> vals;
+                if (ST.containsKey(k)) {
+                    vals = ST.get(k);
+                }
+                else {
+                    vals = new LinkedList<>();
+                    ST.put(k, vals);
+                }
+                vals.add(i);
+            }
         }
         keys.trimToSize();
-        G = new Digraph(keys.size());
+    }
+
+    private void parseHypernyms (String file) {
         // Parse hypernyms
-        In hypFile = new In(hypernyms);
+        In hypFile = new In(file);
         while (hypFile.hasNextLine()) {
             String[] line = hypFile.readLine().split(",");
             int v = Integer.parseInt(line[0]);
@@ -49,90 +101,18 @@ public class WordNet {
         }
     }
 
-    // returns all WordNet nouns
-    public Iterable<String> nouns() { return ST.keySet(); }
-
-    // is the word a WordNet noun?
-    public boolean isNoun(String word) {
-        isNull(word);
-        return ST.containsKey(word);
-    }
-
-    // distance between nounA and nounB (defined below)
-    public int distance(String nounA, String nounB) {
-        nounError(nounA, nounB);
-        int[] pathA = rootPath( ST.get(nounA) ).toArray();
-        int[] pathB = rootPath( ST.get(nounB) ).toArray();
-        int iA = pathA.length - 1;
-        int iB = pathB.length - 1;
-        if (pathA[iA--] != pathB[iB--]) { return -1; }
-        while (iA >= 0 && iB >= 0) {
-            if (pathA[iA] != pathB[iB]) { break; }
-            iA--; iB--;
-        }
-        iA++; iB++;
-        return iA + iB ;
-    }
-
-    // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
-    // in a shortest ancestral path (defined below)
-    public String sap(String nounA, String nounB) {
-        int[] pathA = rootPath( ST.get(nounA) ).toArray();
-        int[] pathB = rootPath( ST.get(nounB) ).toArray();
-        int iA = pathA.length - 1;
-        int iB = pathB.length - 1;
-        if (pathA[iA--] != pathB[iB--]) { return ""; }
-        while (iA >= 0 && iB >= 0) {
-            if (pathA[iA] != pathB[iB]) { break; }
-            --iA; --iB;
-        }
-        // need to move 1 step before fault was detected
-        iA ++;
-        return keys.get(pathA[iA]);
-    }
-
     private void nounError(String a, String b) {
-        isNull(a);
-        isNull(b);
+        errorOnNull(a); errorOnNull(b);
         if (!isNoun(a) || !isNoun(b)) {
             throw new IllegalArgumentException("Recieved a noun input which is not in the WordNet.");
         }
     }
 
-    private void isNull(Object obj) {
+    private void errorOnNull(Object obj) {
         if (obj == null) {
             throw new IllegalArgumentException("Received a null input.");
         }
     }
-
-    private IntStream rootPath(int v) {
-        IntStream.Builder path = IntStream.builder();
-        for (Iterable<Integer> adj = G.adj(v); G.outdegree(v) > 0; adj = G.adj(v)) {
-            path.add(v);
-            ArrayList<Integer> adjLst = new ArrayList<>();
-            adj.iterator().forEachRemaining(adjLst::add);
-            if (adjLst.size() == 1) { v = adjLst.get(0); }
-            else {
-                IntStream shortest = null;
-                for (int w : adjLst ) {
-                    IntStream cur = rootPath(w);
-                    if (shortest == null || cur.count() < shortest.count()) { shortest = cur; }
-                }
-                // despite IDEA warnings shortest cannot be null because adjList.size() > 1
-                return IntStream.concat(shortest, path.build());
-            }
-        }
-        path.add(v);
-        return path.build();
-    }
-
-    private int nounToIndex(String noun) {
-        Object idx = ST.get(noun);
-        if (idx == null) { throw new IllegalArgumentException("Couldn't find noun: " + noun+ "."); }
-    return (int) idx;
-    }
-
-
 
     // do unit testing of this class
     public static void main(String[] args) {
@@ -146,10 +126,6 @@ public class WordNet {
         String noun1 = "horse Equus_caballus";
         //String noun2 = "lion";
         String noun2 = "lion king_of_beasts Panthera_leo";
-        int[] path1 = WN.rootPath(WN.nounToIndex(noun1)).toArray();
-        int[] path2 = WN.rootPath(WN.nounToIndex(noun2)).toArray();
-        System.out.printf("Path %30s: %s%n", noun1, Arrays.toString(path1));
-        System.out.printf("Path %30s: %s%n", noun2, Arrays.toString(path2));
         System.out.printf("Mutual parent of %1$s and %2$s: %3$s.%n", noun1, noun2, WN.sap(noun1, noun2)  );
         System.out.printf( "Distance between %1$s and %2$s: %3$s,%n", noun1, noun2, WN.distance(noun1, noun2) ) ;
 
