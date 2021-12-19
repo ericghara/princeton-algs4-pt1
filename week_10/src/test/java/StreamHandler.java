f6d4efbf89a861401029a4bb2f7aa459429cee55
ericghara <eric.gharakhanian@gmail.com>
2021-12-18 19:50:24 -0800

    StreamHandler comments

--- week_10/src/test/java/StreamHandler.java
+++ week_10/src/test/java/StreamHandler.java
@@ -3,6 +3,10 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
 
+/**
+ * A collection of static methods which facilitate parameterized testing of methods which rely on reading and writing
+ * from an {@code InputStream} to an {@code PrintStream}.
+ */
 public class StreamHandler {
 
     static final InputStream inBackup = System.in;
@@ -15,7 +19,7 @@ public class StreamHandler {
      * @param input String to convert to a {@code ByteArrayInputStream}
      * @return an empty {@code ByteArrayOutputStream}
      */
-    static ByteArrayOutputStream streamInStreamOut(String input) {
+    public static ByteArrayOutputStream streamInStreamOut(String input) {
         ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes() );
         System.setIn(in);
         java.io.ByteArrayOutputStream out = new java.io.ByteArrayOutputStream();
@@ -23,6 +27,10 @@ public class StreamHandler {
         return out;
     }
 
+
+    /**
+     * Sets System input and output streams to their initial targets (i.e. when {@code SteamHandler} [i]class[/i] was loaded).
+     */
     public static void resetAll() {
         System.setIn(inBackup);
         System.setOut(outBackup);
